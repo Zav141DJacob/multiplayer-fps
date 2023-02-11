3dd19b514facab42b4af04b5bd24f29134d09bf4
Alex V <roosarula@gmail.com>
2023-02-11 16:35:49 +0200

    rebase retry

--- client/src/game/mod.rs
+++ client/src/game/mod.rs
@@ -2,12 +2,13 @@ mod pixels;
 mod minimap;
 mod utils;
 
+use std::f32::consts::PI;
 use std::fmt::{Display, Formatter};
 use hecs::Entity;
 use notan::app::{App, Color, Graphics, Plugins};
-use notan::draw::{CreateDraw, DrawImages, DrawTransform};
+use notan::draw::{CreateDraw, DrawImages, DrawTransform, DrawShapes};
 use notan::egui::{DragValue, EguiPluginSugar, Grid, Slider, Ui, Widget, Window};
-use notan::prelude::{Assets, Texture, KeyCode};
+use notan::prelude::*;
 use common::map::Map;
 use crate::game::utils::*;
 use crate::game::pixels::Pixels;
@@ -128,10 +129,32 @@ impl ProgramState for Game {
         let p = self.world.query_one_mut::<&mut Player>(self.player).unwrap();
 
         // Draw a red dot
-        let (width, height) = self.pixels.dimensions();
         let x = p.x;
         let y = p.y;
         self.pixels.set_color(x as usize, y as usize, Color::RED);
+
+        // Draw canvas background
+        let (width, height) = self.pixels.dimensions();
+
+        // init map
+        const MAP_W: usize = 16; // map width
+        const MAP_H: usize = 16; // map height
+        const MAP: &str = "0000222222220000\
+            1              0\
+            1      11111   0\
+            1     0        0\
+            0     0  1110000\
+            0     3        0\
+            0   10000      0\
+            0   0   11100  0\
+            0   0   0      0\
+            0   0   1  00000\
+            0       1      0\
+            2       1      0\
+            0       0      0\
+            0 0000000      0\
+            0              0\
+            0002222222200000";
 
         // Render pixels
         self.pixels.flush(gfx);
@@ -139,6 +162,61 @@ impl ProgramState for Game {
 
         let mut draw = gfx.create_draw();
         draw.image(self.pixels.texture()).scale(1.0, 1.0);
+        // Render map
+        let rect_w = (width as f32 / (MAP_W*2) as f32);
+        let rect_h = (height as f32 / MAP_H as f32);
+        for j in 0..MAP_H {
+            // draw the map
+            for i in 0..MAP_W {
+                if MAP.as_bytes()[i + j * MAP_W] as char == ' ' {
+                    continue; // skip empty spaces
+                }
+                let rect_x = (i as f32) * rect_w;
+                let rect_y = (j as f32) * rect_h;
+                draw.rect((rect_x, rect_y), (rect_w, rect_h));
+            }
+        }
+
+        // init player and draw them on canvas
+        // Coord
+        let player_x = 3.456; // player x position
+        let player_y = 2.345;
+        // Direction
+        let player_a: f32 = 1.523; // player view direction
+        const FOV: f32 = PI / 3.;
+        draw.rect((player_x * rect_w, player_y * rect_h), (5., 5.));
+
+        // DRAW FOV RAYCAST
+        for i in 0..width {
+            let mut t = 0.;
+            // draw the visibility cone
+            let angle = player_a - FOV / 2. + FOV * i as f32 / width as f32;
+            let mut m = 0.;
+            let mut n = 0.;
+            while t < 20. {
+                t = t + 0.05;
+                let cx = player_x + t * angle.cos();
+                let cy = player_y + t * angle.sin();
+                if (MAP.as_bytes()[(cx as usize + cy as usize * MAP_W) as usize] as char != ' ') {
+                    break;
+                };
+
+                let pix_x = cx * rect_w;
+                let pix_y = cy * rect_h;
+
+                m = pix_x;
+                n = pix_y;
+            }
+            draw.path()
+                .move_to(player_x * rect_w, player_y * rect_h)
+                .line_to(m, n)
+                .color(Color::WHITE)
+                .stroke(0.05);
+        }
+        // Render pixels
+        self.pixels.flush(gfx);
+        self.pixels.clear(Color::BLACK);
+
 
         // Drawing minimap
         self.minimap.draw(&mut draw, width, height);
