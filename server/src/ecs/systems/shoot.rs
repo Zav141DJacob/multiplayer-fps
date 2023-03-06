6770ab86db55d2a02f09c8530d31c3e0755c4db9
Olari Tšernobrovkin <olaritserno@gmail.com>
2023-03-06 03:32:01 +0200

    Implemented the new timer struct into Georgi's shooting code

--- server/src/ecs/systems/shoot.rs
+++ server/src/ecs/systems/shoot.rs
@@ -1,15 +1,16 @@
 use crate::ecs::spawn::bullet::spawn_bullet;
 use crate::ecs::systems::ServerSystems;
 use crate::ecs::ServerEcs;
-use common::ecs::components::{ Bullet, HeldWeapon, InputState, LookDirection, Position, ShootCooldown };
-use hecs::Entity;
-use std::time::{Duration, SystemTime};
+use common::ecs::components::{ HeldWeapon, InputState, LookDirection, Position };
+use std::time::Duration;
+use common::ecs::timer::Timer;
+use crate::ecs::components::{BulletDespawn, ShootCooldown};
 
 struct BulletSpawn {
     pos: Position,
     dir: LookDirection,
     range: f32,
-    despawn: SystemTime,
+    duration: Duration,
 }
 
 impl ServerSystems {
@@ -18,10 +19,10 @@ impl ServerSystems {
         let query = ecs
             .world
             .query_mut::<(&InputState, &LookDirection, &Position, &HeldWeapon)>()
-            .without::<&ShootCooldown>();
+            .without::<&Timer<ShootCooldown>>();
 
-        let mut bullets: Vec<BulletSpawn> = Vec::new();
-        let mut cooldowns: Vec<(Entity, ShootCooldown)> = Vec::new();
+        let mut bullets = Vec::new();
+        let mut cooldowns = Vec::new();
 
         for (entity, (input, look_dir, position, weapon)) in query {
             // Check if shooting
@@ -31,60 +32,35 @@ impl ServerSystems {
 
             // Spawn bullet
             let range = weapon.0.range();
-            let despawn = SystemTime::now() + Duration::from_secs_f32(1.0);
+            let duration = Duration::from_secs_f32(1.0);
             bullets.push(BulletSpawn {
-                pos: position.clone(),
-                dir: look_dir.clone(),
-                range: range,
-                despawn,
+                pos: *position,
+                dir: *look_dir,
+                range,
+                duration,
             });
 
             // Set cooldown
             let cooldown = weapon.0.recharge();
-            let cooldown_end = SystemTime::now() + cooldown;
-            cooldowns.push((entity, ShootCooldown(cooldown_end)));
+            cooldowns.push((entity, Timer::new(cooldown, ShootCooldown)));
         }
 
         for bullet in bullets {
-            spawn_bullet(ecs, bullet.pos, bullet.dir, bullet.range, bullet.despawn);
+            spawn_bullet(ecs, bullet.pos, bullet.dir, bullet.range, bullet.duration);
         }
 
-        for cooldown in cooldowns {
-            ecs.world.insert_one(cooldown.0, cooldown.1).unwrap();
+        for (entity, cooldown) in cooldowns {
+            ecs.world.insert_one(entity, cooldown).unwrap();
         }
     }
 
     pub fn shoot_cooldown_system(ecs: &mut ServerEcs, _dt: f32) {
-        let mut to_remove = vec![];
-        let now = SystemTime::now();
-
-        ecs.world
-            .query_mut::<&ShootCooldown>()
-            .into_iter()
-            .filter(|(_, cooldown)| cooldown.0 < now)
-            .for_each(|(entity, _)| {
-                to_remove.push(entity);
-            });
-
-        for entity in to_remove {
-            ecs.world.remove_one::<&ShootCooldown>(entity).unwrap();
-        }
+        Timer::<ShootCooldown>::system(&mut ecs.world);
     }
 
     pub fn bullet_despawn_system(ecs: &mut ServerEcs, _dt: f32) {
-        let mut to_remove = vec![];
-        let now = SystemTime::now();
-
-        ecs.world
-            .query_mut::<&Bullet>()
-            .into_iter()
-            .filter(|(_, bullet)| bullet.despawn_time < now)
-            .for_each(|(entity, _)| {
-                to_remove.push(entity);
-            });
-
-        for entity in to_remove {
-            ecs.world.despawn(entity).unwrap();
-        }
+        Timer::<BulletDespawn>::system_with(&mut ecs.world, |world, entity, _| {
+            ecs.observer.observe(world).despawn(entity).unwrap();
+        });
     }
 }
