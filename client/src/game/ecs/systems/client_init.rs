6d90ad98f83e7e510658f2461fb537b21ceab77d
Olari Tšernobrovkin <olaritserno@gmail.com>
2023-03-16 11:43:36 +0200

    Refactored crate rendering to use existing systems

--- client/src/game/ecs/systems/client_init.rs
+++ client/src/game/ecs/systems/client_init.rs
@@ -1,14 +1,20 @@
 use glam::Vec2;
 use itertools::Itertools;
-use common::ecs::components::{Bullet, Player};
+use common::ecs::components::{Bullet, Player, WeaponCrate};
 use crate::game::ecs::ClientEcs;
 use crate::game::ecs::component::{ClientInitialized, Height, RenderSprite, Scale};
 use crate::game::ecs::systems::ClientSystems;
-use crate::game::texture::{ANIMATED_PLAYER, TEX_BULLET};
+use crate::game::texture::{ANIMATED_PLAYER, TEX_BULLET, WEAPON_CRATE};
 
 impl ClientSystems {
+    pub fn client_init(ecs: &mut ClientEcs, _dt: f32) {
+        Self::init_player(ecs);
+        Self::init_bullet(ecs);
+        Self::init_crate(ecs);
+    }
+
     /// Move all entities with a position and velocity
-    pub fn init_player(ecs: &mut ClientEcs, _dt: f32) {
+    fn init_player(ecs: &mut ClientEcs) {
         let entities = ecs.world.query_mut::<()>().with::<&Player>().without::<&ClientInitialized>()
             .into_iter()
             .map(|(ent, _)| ent)
@@ -22,7 +28,7 @@ impl ClientSystems {
         }
     }
 
-    pub fn init_bullet(ecs: &mut ClientEcs, _dt: f32) {
+    fn init_bullet(ecs: &mut ClientEcs) {
         let entities = ecs.world.query_mut::<()>().with::<&Bullet>().without::<&ClientInitialized>()
             .into_iter()
             .map(|(ent, _)| ent)
@@ -34,6 +40,21 @@ impl ClientSystems {
                 RenderSprite { tex: &TEX_BULLET },
                 Scale(Vec2::splat(BULLET_SCALE)),
                 Height(0.5 - BULLET_SCALE / 2.0),
+                ClientInitialized,
+            )).unwrap();
+        }
+    }
+
+    fn init_crate(ecs: &mut ClientEcs) {
+        let entities = ecs.world.query_mut::<()>().with::<&WeaponCrate>().without::<&ClientInitialized>()
+            .into_iter()
+            .map(|(ent, _)| ent)
+            .collect_vec();
+
+        for entity in entities {
+            ecs.world.insert(entity, (
+                RenderSprite { tex: &WEAPON_CRATE },
+                Scale(Vec2::splat(0.5)),
                 ClientInitialized,
             )).unwrap();
         }
